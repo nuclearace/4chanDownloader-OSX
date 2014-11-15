73722d2f99cfd8e8bdd454783b89692d52186ed3
Erik <nuclear.ace@gmail.com>
2014-11-15 08:59:46 -0500

    Initial Commit

---
+++ 4chan Image Downloader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  4chan Image Downloader
+//
+//  Created by Erik Little on 11/15/14.
+//  Copyright (c) 2014 Erik Little. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 4chan Image Downloader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  4chan Image Downloader
+//
+//  Created by Erik Little on 11/15/14.
+//  Copyright (c) 2014 Erik Little. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
