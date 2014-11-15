1f6853de5a5bbe88bdd172a5e95c53ef5acae217
Erik <nuclear.ace@gmail.com>
2014-11-15 17:36:51 -0500

    It works for the most part

---
+++ 4chan Image Downloader/ChanDownloader.swift
@@ -0,0 +1,135 @@
+//
+//  Downloader.swift
+//  4chan Image Downloader
+//
+//  Created by Erik Little on 11/15/14.
+//  Copyright (c) 2014 Erik Little. All rights reserved.
+//
+
+import Foundation
+
+class ChanDownloader: NSObject {
+    weak var downloadView:ThreadDownloaderController!
+    let thread:String!
+    let manager = NSFileManager()
+    var board:String!
+    var downloadPath:NSURL!
+    var downloadTimer:NSTimer!
+    var getNum = 0
+    var numPosts = 0
+    var pendingRequests = Array<() -> Void>()
+    var posts:NSArray!
+    var threadNumber:String!
+    
+    init(thread:String, downloadPath:NSURL, downloadView:ThreadDownloaderController) {
+        super.init()
+        println("Should init with thread: \(thread)")
+        self.downloadView = downloadView
+        self.thread = thread
+        self.downloadPath = downloadPath
+        self.retrieveThreadJSON()
+    }
+    
+    deinit {
+        println("Downloader is being released")
+    }
+    
+    func executeGet() {
+        self.pendingRequests[self.getNum]()
+        self.getNum++
+    }
+    
+    func getImages() {
+        println("Getting thread images")
+        self.numPosts = 0
+        
+        // Creates a function that can be called to download the image
+        func createFun(filename:String, ext:String, tim:String, num:Int) -> () -> Void  {
+            // The download function
+            func getImage() {
+                var lastImage = false
+                self.downloadView.appendTextAndScroll("GET: " + tim + ext + "\n")
+                if (num == self.numPosts) {
+                    lastImage = true
+                    println("Invalidating timer")
+                    self.downloadTimer.invalidate()
+                    self.getNum = 0
+                    self.pendingRequests.removeAll(keepCapacity: false)
+                }
+                let imageURL = NSString(format: "https://i.4cdn.org/%@/%@%@", self.board, tim, ext)
+                let req = NSURLRequest(URL: NSURL(string: imageURL)!)
+                NSURLConnection.sendAsynchronousRequest(req, queue: NSOperationQueue()) {[unowned self] res, data, err in
+                    self.downloadView.appendTextAndScroll("GOT: " + tim + ext + "\n")
+                    self.manager.createFileAtPath(self.downloadPath.path! + "/" + tim + ext,
+                        contents: data, attributes: nil)
+                    if (lastImage) {
+                        self.downloadView.appendTextAndScroll("DONE")
+                        NSNotificationCenter.defaultCenter().postNotificationName("done", object: nil)
+                        return
+                    }
+                    
+                }
+            }
+            return getImage
+        }
+        
+        for (var i = 0; i < posts.count; i++) {
+            if let filename = posts[i]["filename"] as? String {
+                let ext = posts[i]["ext"] as String
+                let tim = String(posts[i]["tim"] as Int)
+                let fun = createFun(filename, ext, tim, self.numPosts + 1)
+                self.pendingRequests.append(fun)
+                self.numPosts++
+            }
+        }
+        self.startTimer()
+    }
+    
+    private func retrieveThreadJSON() {
+        if (!self.verifyTheadURL()) {
+            NSNotificationCenter.defaultCenter().postNotificationName("invalidThread", object: nil)
+            return
+        }
+        let newPath = "file:///" + self.downloadPath.path! + "/4chanDownloads/"
+            + self.board + "/" + self.threadNumber
+        self.downloadPath = NSURL(string: newPath)
+        println(self.downloadPath)
+        var err:NSError?
+        manager.createDirectoryAtURL(self.downloadPath, withIntermediateDirectories: true,
+            attributes: nil, error: &err)
+        if (err != nil) {
+            println(err?.localizedDescription)
+            return
+        }
+        println("Getting thread JSON")
+        let requestString = NSString(format: "http://a.4cdn.org/%@/thread/%@.json", self.board, self.threadNumber)
+        let request = NSURLRequest(URL: NSURL(string: requestString)!)
+        var jsonError:NSError?
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue()) {[unowned self] res, data, err in
+            var realJSON = NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: &jsonError) as NSDictionary
+            if let posts = realJSON["posts"] as? NSArray {
+                self.posts = posts
+                self.getImages()
+            }
+        }
+    }
+    
+    private func startTimer() {
+        dispatch_async(dispatch_get_main_queue()) {[unowned self] in
+            self.downloadTimer = NSTimer.scheduledTimerWithTimeInterval(1, target: self,
+                selector: Selector("executeGet"), userInfo: nil, repeats: true)
+        }
+    }
+    
+    private func verifyTheadURL() -> Bool {
+        let mutRegex = RegexMutable(self.thread)
+        let matches = mutRegex["https?\\:\\/\\/boards\\.4chan\\.org\\/(.*)\\/thread\\/(\\d*)"].matches()
+        let groups = mutRegex["https?\\:\\/\\/boards\\.4chan\\.org\\/(.*)\\/thread\\/(\\d*)"].groups()
+        if (matches.count == 0) {
+            return false
+        }
+        self.board = groups[1] as String
+        self.threadNumber = groups[2] as String
+        return true
+    }
+}
---
+++ 4chan Image Downloader/ThreadDownloaderController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  4chan Image Downloader
+//
+//  Created by Erik Little on 11/15/14.
+//  Copyright (c) 2014 Erik Little. All rights reserved.
+//
+
+import Cocoa
+
+class ThreadDownloaderController: NSViewController  {
+    
+    @IBOutlet weak var downloadButton:NSButton!
+    @IBOutlet weak var threadTextField:NSTextField!
+    @IBOutlet var downloadTextView: NSTextView!
+    
+    var downloader:ChanDownloader!
+    var posts = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleDone:", name: "done", object: nil)
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    override var representedObject:AnyObject? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    @IBAction func downloadWasClicked(sender:AnyObject) {
+        self.downloadTextView.string = ""
+        let thread = threadTextField.stringValue
+        let openPanel = NSOpenPanel()
+        openPanel.canChooseDirectories = true
+        openPanel.canChooseFiles = false
+        openPanel.allowsMultipleSelection = false
+        var clicked = openPanel.runModal()
+        
+        if (clicked == NSFileHandlingPanelOKButton) {
+            self.appendTextAndScroll("Starting Download\n")
+            self.downloader = ChanDownloader(thread: thread,
+                downloadPath: openPanel.URL!, downloadView: self)
+            self.threadTextField.setValue("", forKey: "stringValue")
+        }
+    }
+    
+    func appendTextAndScroll(text:String) {
+        dispatch_async(dispatch_get_main_queue()) {[unowned self] in
+            var string = NSAttributedString(string: text)
+            self.downloadTextView.textStorage?.appendAttributedString(string)
+            self.downloadTextView.scrollRangeToVisible(NSMakeRange(countElements(self.downloadTextView.string!), 0))
+        }
+    }
+    
+    func handleDone(not:NSNotification) {
+        println("handling done")
+        dispatch_async(dispatch_get_main_queue()) {[unowned self] in
+            self.downloader = nil
+        }
+    }
+}
+
